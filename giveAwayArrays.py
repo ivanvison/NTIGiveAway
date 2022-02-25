e6eb425990ca2cc6851143d1e2f7c6ae324c0161
ivanvison <ivanvison@gmail.com>
2022-02-24 22:14:41 -0400

    updated array

--- giveAwayArrays.py
+++ giveAwayArrays.py
@@ -1,4 +1,3 @@
 #Filling the Arrays with Cool Stuff
 coolNames = ['Hugo', 'Bryan', 'Sugeiri', 'Albert', 'Tomas', 'Angel', 'Luis', 'Evan', 'Wandy', 'Judith', 'Hector', 'Yansell', 'Juan Diego', 'Luis Alvarez', 'Ana', 'Mendi', 'Enercido']
-coolNames = ['Hugo','Bryan', 'Sugeiri', 'Albert', 'Tomas', 'Angel', 'Luis', 'Evan', 'Wandy', 'Judith', 'Hector', 'Yansell', 'Juan Diego']
 coolMsgs = ['tumba eso!!!', 'una bullaaaa!!!!!', 'eyyyyyyyy!!!!', 'k lo waaaa!!!!', 'allaaa lo wawawaaaa!!!', 'eso e mio!!!', 'la potalitaaaaa!!!']
